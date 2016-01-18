d05f3a09ec13d05424ea8de453bd850da3d3ead0
Jister <chenjiesjtu14@gmail.com>
2016-01-18 13:44:01 +0800

    new repository

---
+++ src/scan_to_cloud.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "laser_geometry/laser_geometry.h"
+
+laser_geometry::LaserProjection projector;
+sensor_msgs::PointCloud cloud;
+
+void scanCallback (const sensor_msgs::LaserScan scan)
+{
+  projector.projectLaser(scan, cloud);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "scan_to_cloud");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/scan", 1, scanCallback);
+  ros::Publisher pub = n.advertise<sensor_msgs::PointCloud>("/pointcloud", 1000);
+  ros::Rate loop_rate(20);
+
+  while(ros::ok())
+  {
+    pub.publish(cloud);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
