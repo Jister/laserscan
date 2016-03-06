49b023a024bd16edcd86ac8a102bd589dcef98b8
Jister <chenjiesjtu14@gmail.com>
2016-03-06 07:22:42 +0000

    crop

--- src/find_min_distance.cpp
+++ src/find_min_distance.cpp
@@ -4,24 +4,23 @@
 #include "laserscan/Laser.h"
 
 laserscan::Laser pos;
-
 void scanCallback(const sensor_msgs::LaserScan laser)
 {
   float min_distance;
   int angle;
 
-  min_distance = laser.ranges[0];
+  min_distance =20.0;
   angle = 0;
-  for(int i=0; i<laser.ranges.size(); i=i+4)
+  for(int i=0; i<laser.ranges.size(); i++)
   {
     if(laser.ranges[i]<min_distance)
     {
       min_distance = laser.ranges[i];
-      angle=i*0.25;
+      angle=i;
     }
   }
   pos.min_distance = min_distance*100;
-  pos.angle = angle-45;
+  pos.angle = angle*0.25 - 45;
 } 
 
 int main(int argc, char **argv)
