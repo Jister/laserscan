0638c611364a744d3262100709b7f936b689bcef
Jister <chenjiesjtu14@gmail.com>
2017-11-27 22:42:16 +0800

    update

--- src/laser_odom.cpp
+++ src/laser_odom.cpp
@@ -55,6 +55,10 @@ private:
 	double a_cos_;
 	double a_sin_;
 
+	sm_params input_;
+	sm_result output_;
+	LDP prev_ldp_scan_;
+
 	Vector3f acc_b;
 	Vector3f acc_w;
 
@@ -162,6 +166,215 @@ void LaserOdometry::laserScanToLDP(const sensor_msgs::LaserScan::ConstPtr& scan_
   ldp->true_pose[0] = 0.0;
   ldp->true_pose[1] = 0.0;
   ldp->true_pose[2] = 0.0;
+}
+
+void LaserOdometry::processScan(LDP& curr_ldp_scan, const ros::Time& time)
+{
+  ros::WallTime start = ros::WallTime::now();
+
+  // CSM is used in the following way:
+  // The scans are always in the laser frame
+  // The reference scan (prevLDPcan_) has a pose of [0, 0, 0]
+  // The new scan (currLDPScan) has a pose equal to the movement
+  // of the laser in the laser frame since the last scan
+  // The computed correction is then propagated using the tf machinery
+
+  prev_ldp_scan_->odometry[0] = 0.0;
+  prev_ldp_scan_->odometry[1] = 0.0;
+  prev_ldp_scan_->odometry[2] = 0.0;
+
+  prev_ldp_scan_->estimate[0] = 0.0;
+  prev_ldp_scan_->estimate[1] = 0.0;
+  prev_ldp_scan_->estimate[2] = 0.0;
+
+  prev_ldp_scan_->true_pose[0] = 0.0;
+  prev_ldp_scan_->true_pose[1] = 0.0;
+  prev_ldp_scan_->true_pose[2] = 0.0;
+
+  input_.laser_ref  = prev_ldp_scan_;
+  input_.laser_sens = curr_ldp_scan;
+
+  // **** estimated change since last scan
+
+  double dt = (time - last_icp_time_).toSec();
+  double pr_ch_x, pr_ch_y, pr_ch_a;
+  getPrediction(pr_ch_x, pr_ch_y, pr_ch_a, dt);
+
+  // the predicted change of the laser's position, in the fixed frame
+
+  tf::Transform pr_ch;
+  createTfFromXYTheta(pr_ch_x, pr_ch_y, pr_ch_a, pr_ch);
+
+  // account for the change since the last kf, in the fixed frame
+
+  pr_ch = pr_ch * (f2b_ * f2b_kf_.inverse());
+
+  // the predicted change of the laser's position, in the laser frame
+
+  tf::Transform pr_ch_l;
+  pr_ch_l = laser_to_base_ * f2b_.inverse() * pr_ch * f2b_ * base_to_laser_ ;
+
+  input_.first_guess[0] = pr_ch_l.getOrigin().getX();
+  input_.first_guess[1] = pr_ch_l.getOrigin().getY();
+  input_.first_guess[2] = tf::getYaw(pr_ch_l.getRotation());
+
+  // If they are non-Null, free covariance gsl matrices to avoid leaking memory
+  if (output_.cov_x_m)
+  {
+    gsl_matrix_free(output_.cov_x_m);
+    output_.cov_x_m = 0;
+  }
+  if (output_.dx_dy1_m)
+  {
+    gsl_matrix_free(output_.dx_dy1_m);
+    output_.dx_dy1_m = 0;
+  }
+  if (output_.dx_dy2_m)
+  {
+    gsl_matrix_free(output_.dx_dy2_m);
+    output_.dx_dy2_m = 0;
+  }
+
+  // *** scan match - using point to line icp from CSM
+
+  sm_icp(&input_, &output_);
+  tf::Transform corr_ch;
+
+  if (output_.valid)
+  {
+
+    // the correction of the laser's position, in the laser frame
+    tf::Transform corr_ch_l;
+    createTfFromXYTheta(output_.x[0], output_.x[1], output_.x[2], corr_ch_l);
+
+    // the correction of the base's position, in the base frame
+    corr_ch = base_to_laser_ * corr_ch_l * laser_to_base_;
+
+    // update the pose in the world frame
+    f2b_ = f2b_kf_ * corr_ch;
+
+    // **** publish
+
+    if (publish_pose_)
+    {
+      // unstamped Pose2D message
+      geometry_msgs::Pose2D::Ptr pose_msg;
+      pose_msg = boost::make_shared<geometry_msgs::Pose2D>();
+      pose_msg->x = f2b_.getOrigin().getX();
+      pose_msg->y = f2b_.getOrigin().getY();
+      pose_msg->theta = tf::getYaw(f2b_.getRotation());
+      pose_publisher_.publish(pose_msg);
+    }
+    if (publish_pose_stamped_)
+    {
+      // stamped Pose message
+      geometry_msgs::PoseStamped::Ptr pose_stamped_msg;
+      pose_stamped_msg = boost::make_shared<geometry_msgs::PoseStamped>();
+
+      pose_stamped_msg->header.stamp    = time;
+      pose_stamped_msg->header.frame_id = fixed_frame_;
+
+      tf::poseTFToMsg(f2b_, pose_stamped_msg->pose);
+
+      pose_stamped_publisher_.publish(pose_stamped_msg);
+    }
+    if (publish_pose_with_covariance_)
+    {
+      // unstamped PoseWithCovariance message
+      geometry_msgs::PoseWithCovariance::Ptr pose_with_covariance_msg;
+      pose_with_covariance_msg = boost::make_shared<geometry_msgs::PoseWithCovariance>();
+      tf::poseTFToMsg(f2b_, pose_with_covariance_msg->pose);
+
+      if (input_.do_compute_covariance)
+      {
+        pose_with_covariance_msg->covariance = boost::assign::list_of
+          (gsl_matrix_get(output_.cov_x_m, 0, 0)) (0)  (0)  (0)  (0)  (0)
+          (0)  (gsl_matrix_get(output_.cov_x_m, 0, 1)) (0)  (0)  (0)  (0)
+          (0)  (0)  (static_cast<double>(position_covariance_[2])) (0)  (0)  (0)
+          (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[0])) (0)  (0)
+          (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[1])) (0)
+          (0)  (0)  (0)  (0)  (0)  (gsl_matrix_get(output_.cov_x_m, 0, 2));
+      }
+      else
+      {
+        pose_with_covariance_msg->covariance = boost::assign::list_of
+          (static_cast<double>(position_covariance_[0])) (0)  (0)  (0)  (0)  (0)
+          (0)  (static_cast<double>(position_covariance_[1])) (0)  (0)  (0)  (0)
+          (0)  (0)  (static_cast<double>(position_covariance_[2])) (0)  (0)  (0)
+          (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[0])) (0)  (0)
+          (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[1])) (0)
+          (0)  (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[2]));
+      }
+
+      pose_with_covariance_publisher_.publish(pose_with_covariance_msg);
+    }
+    if (publish_pose_with_covariance_stamped_)
+    {
+      // stamped Pose message
+      geometry_msgs::PoseWithCovarianceStamped::Ptr pose_with_covariance_stamped_msg;
+      pose_with_covariance_stamped_msg = boost::make_shared<geometry_msgs::PoseWithCovarianceStamped>();
+
+      pose_with_covariance_stamped_msg->header.stamp    = time;
+      pose_with_covariance_stamped_msg->header.frame_id = fixed_frame_;
+
+      tf::poseTFToMsg(f2b_, pose_with_covariance_stamped_msg->pose.pose);
+
+      if (input_.do_compute_covariance)
+      {
+        pose_with_covariance_stamped_msg->pose.covariance = boost::assign::list_of
+          (gsl_matrix_get(output_.cov_x_m, 0, 0)) (0)  (0)  (0)  (0)  (0)
+          (0)  (gsl_matrix_get(output_.cov_x_m, 0, 1)) (0)  (0)  (0)  (0)
+          (0)  (0)  (static_cast<double>(position_covariance_[2])) (0)  (0)  (0)
+          (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[0])) (0)  (0)
+          (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[1])) (0)
+          (0)  (0)  (0)  (0)  (0)  (gsl_matrix_get(output_.cov_x_m, 0, 2));
+      }
+      else
+      {
+        pose_with_covariance_stamped_msg->pose.covariance = boost::assign::list_of
+          (static_cast<double>(position_covariance_[0])) (0)  (0)  (0)  (0)  (0)
+          (0)  (static_cast<double>(position_covariance_[1])) (0)  (0)  (0)  (0)
+          (0)  (0)  (static_cast<double>(position_covariance_[2])) (0)  (0)  (0)
+          (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[0])) (0)  (0)
+          (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[1])) (0)
+          (0)  (0)  (0)  (0)  (0)  (static_cast<double>(orientation_covariance_[2]));
+      }
+
+      pose_with_covariance_stamped_publisher_.publish(pose_with_covariance_stamped_msg);
+    }
+
+    if (publish_tf_)
+    {
+      tf::StampedTransform transform_msg (f2b_, time, fixed_frame_, base_frame_);
+      tf_broadcaster_.sendTransform (transform_msg);
+    }
+  }
+  else
+  {
+    corr_ch.setIdentity();
+    ROS_WARN("Error in scan matching");
+  }
+
+  // **** swap old and new
+
+  if (newKeyframeNeeded(corr_ch))
+  {
+    // generate a keyframe
+    ld_free(prev_ldp_scan_);
+    prev_ldp_scan_ = curr_ldp_scan;
+    f2b_kf_ = f2b_;
+  }
+  else
+  {
+    ld_free(curr_ldp_scan);
+  }
+
+  last_icp_time_ = time;
+
+  // **** statistics
+
+  double dur = (ros::WallTime::now() - start).toSec() * 1e3;
+  ROS_DEBUG("Scan matcher total duration: %.1f ms", dur);
 }
 
 void LaserOdometry::createCache (const sensor_msgs::LaserScan::ConstPtr& scan_msg, double& theta)
