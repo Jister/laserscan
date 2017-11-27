57da07972dfafbf0dec76012e42dad3a6c8645bc
Jister <chenjiesjtu14@gmail.com>
2017-11-27 10:27:45 +0800

    csm

--- src/laser_odom.cpp
+++ src/laser_odom.cpp
@@ -1,18 +1,32 @@
 #include "ros/ros.h"
 
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include "laser_geometry/laser_geometry.h"
-#include "sensor_msgs/LaserScan.h"
-#include "sensor_msgs/Imu.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "sensor_msgs/PointCloud.h"
 #include "sensor_msgs/point_cloud_conversion.h"
 
+#include <csm/csm_all.h>  // csm defines min and max, but Eigen complains
+#undef min
+#undef max
+
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
-
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_types.h> 
-#include <pcl/registration/icp.h> //ICP(iterative closest point)
 
 using namespace std;
 using namespace Eigen;
@@ -38,15 +52,20 @@ private:
 	double pitch;
 	double yaw;
 
+	double a_cos_;
+	double a_sin_;
+
 	Vector3f acc_b;
 	Vector3f acc_w;
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud_previous;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud_current;
 
-	bool is_scan_valid(const sensor_msgs::LaserScan &scan);
+	bool is_scan_valid(const sensor_msgs::LaserScan::ConstPtr& scan);
 
-	void scanCallback(const sensor_msgs::LaserScan& scan);
+	void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+	void laserScanToLDP(const sensor_msgs::LaserScan::ConstPtr& scan_msg, LDP& ldp);
+	void createCache (const sensor_msgs::LaserScan::ConstPtr& scan_msg, double& theta);
 	void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg);
 
 };
@@ -71,37 +90,15 @@ LaserOdometry::~LaserOdometry()
 {
 }
 
-void LaserOdometry::scanCallback(const sensor_msgs::LaserScan& scan)
+void LaserOdometry::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-	laser_geometry::LaserProjection projector;
-	sensor_msgs::PointCloud cloud;
-	sensor_msgs::PointCloud2 cloud2;
-	//convert LaserScan to PointCloud
-	projector.projectLaser(scan, cloud);
-	//convert PointCloud to PointCloud2
-	sensor_msgs::convertPointCloudToPointCloud2(cloud, cloud2);
-
 	if(!initialized)
 	{
-		pcl::fromROSMsg(cloud2, *pcl_cloud_previous);
 		initialized = true;
 	}else
 	{
-		pcl::fromROSMsg(cloud2, *pcl_cloud_current);
-		//*********************************
-		// ICP
-		//*********************************
-		pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp; 
-		icp.setInputSource(pcl_cloud_previous); //set the input cloud
-		icp.setInputTarget(pcl_cloud_current); //set the output cloud
-		pcl::PointCloud<pcl::PointXYZ> final; //result
-		//进行配准，结果存储在Final中
-		icp.align(final); 
-		//输出ICP配准的信息（是否收敛，拟合度）
-		std::cout << "has converged:" << icp.hasConverged() << " score: " <<
-		icp.getFitnessScore() << std::endl;
-		//输出最终的变换矩阵（4x4）
-		std::cout << icp.getFinalTransformation() << std::endl;
+		createCache(scan, yaw);
+		
 
 	}
 }
@@ -126,6 +123,59 @@ void LaserOdometry::imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg)
 	acc_w(2) = acc_world.getZ();
 }
 
+void LaserOdometry::laserScanToLDP(const sensor_msgs::LaserScan::ConstPtr& scan_msg, LDP& ldp)
+{
+  unsigned int n = scan_msg->ranges.size();
+  ldp = ld_alloc_new(n);
+
+  for (unsigned int i = 0; i < n; i++)
+  {
+    // calculate position in laser frame
+
+    double r = scan_msg->ranges[i];
+
+    if (r > scan_msg->range_min && r < scan_msg->range_max)
+    {
+      // fill in laser scan data
+
+      ldp->valid[i] = 1;
+      ldp->readings[i] = r;
+    }
+    else
+    {
+      ldp->valid[i] = 0;
+      ldp->readings[i] = -1;  // for invalid range
+    }
+
+    ldp->theta[i]    = scan_msg->angle_min + i * scan_msg->angle_increment;
+
+    ldp->cluster[i]  = -1;
+  }
+
+  ldp->min_theta = ldp->theta[0];
+  ldp->max_theta = ldp->theta[n-1];
+
+  ldp->odometry[0] = 0.0;
+  ldp->odometry[1] = 0.0;
+  ldp->odometry[2] = 0.0;
+
+  ldp->true_pose[0] = 0.0;
+  ldp->true_pose[1] = 0.0;
+  ldp->true_pose[2] = 0.0;
+}
+
+void LaserOdometry::createCache (const sensor_msgs::LaserScan::ConstPtr& scan_msg, double& theta)
+{
+  a_cos_.clear();
+  a_sin_.clear();
+
+  for (unsigned int i = 0; i < scan_msg->ranges.size(); ++i)
+  {
+    double angle = scan_msg->angle_min + i * scan_msg->angle_increment + theta;
+    a_cos_.push_back(cos(angle));
+    a_sin_.push_back(sin(angle));
+  }
+}
 
 int main(int argc, char **argv)
 {
