1564a92faedc372e1829bf8b7377afb6ccc288b7
Jister <chenjiesjtu14@gmail.com>
2017-12-17 11:44:45 +0800

    line extraction

--- src/laser_odom.cpp
+++ src/laser_odom.cpp
@@ -88,6 +88,124 @@ LaserOdometry::LaserOdometry(ros::NodeHandle n):
 		use_imu = false;
 	}
 
+	// **** CSM parameters - comments copied from algos.h (by Andrea Censi)
+
+	// Maximum angular displacement between scans
+	if (!nh.getParam ("max_angular_correction_deg", input_.max_angular_correction_deg))
+		input_.max_angular_correction_deg = 45.0;
+
+	// Maximum translation between scans (m)
+	if (!nh.getParam ("max_linear_correction", input_.max_linear_correction))
+		input_.max_linear_correction = 0.50;
+
+	// Maximum ICP cycle iterations
+	if (!nh.getParam ("max_iterations", input_.max_iterations))
+		input_.max_iterations = 10;
+
+	// A threshold for stopping (m)
+	if (!nh.getParam ("epsilon_xy", input_.epsilon_xy))
+		input_.epsilon_xy = 0.000001;
+
+	// A threshold for stopping (rad)
+	if (!nh.getParam ("epsilon_theta", input_.epsilon_theta))
+		input_.epsilon_theta = 0.000001;
+
+	// Maximum distance for a correspondence to be valid
+	if (!nh.getParam ("max_correspondence_dist", input_.max_correspondence_dist))
+		input_.max_correspondence_dist = 0.3;
+
+	// Noise in the scan (m)
+	if (!nh.getParam ("sigma", input_.sigma))
+		input_.sigma = 0.010;
+
+	// Use smart tricks for finding correspondences.
+	if (!nh.getParam ("use_corr_tricks", input_.use_corr_tricks))
+		input_.use_corr_tricks = 1;
+
+	// Restart: Restart if error is over threshold
+	if (!nh.getParam ("restart", input_.restart))
+		input_.restart = 0;
+
+	// Restart: Threshold for restarting
+	if (!nh.getParam ("restart_threshold_mean_error", input_.restart_threshold_mean_error))
+		input_.restart_threshold_mean_error = 0.01;
+
+	// Restart: displacement for restarting. (m)
+	if (!nh.getParam ("restart_dt", input_.restart_dt))
+		input_.restart_dt = 1.0;
+
+	// Restart: displacement for restarting. (rad)
+	if (!nh.getParam ("restart_dtheta", input_.restart_dtheta))
+		input_.restart_dtheta = 0.1;
+
+	// Max distance for staying in the same clustering
+	if (!nh.getParam ("clustering_threshold", input_.clustering_threshold))
+		input_.clustering_threshold = 0.25;
+
+	// Number of neighbour rays used to estimate the orientation
+	if (!nh.getParam ("orientation_neighbourhood", input_.orientation_neighbourhood))
+		input_.orientation_neighbourhood = 20;
+
+	// If 0, it's vanilla ICP
+	if (!nh.getParam ("use_point_to_line_distance", input_.use_point_to_line_distance))
+		input_.use_point_to_line_distance = 1;
+
+	// Discard correspondences based on the angles
+	if (!nh.getParam ("do_alpha_test", input_.do_alpha_test))
+		input_.do_alpha_test = 0;
+
+	// Discard correspondences based on the angles - threshold angle, in degrees
+	if (!nh.getParam ("do_alpha_test_thresholdDeg", input_.do_alpha_test_thresholdDeg))
+		input_.do_alpha_test_thresholdDeg = 20.0;
+
+	// Percentage of correspondences to consider: if 0.9,
+	// always discard the top 10% of correspondences with more error
+	if (!nh.getParam ("outliers_maxPerc", input_.outliers_maxPerc))
+		input_.outliers_maxPerc = 0.90;
+
+	// Parameters describing a simple adaptive algorithm for discarding.
+	//  1) Order the errors.
+	//  2) Choose the percentile according to outliers_adaptive_order.
+	//     (if it is 0.7, get the 70% percentile)
+	//  3) Define an adaptive threshold multiplying outliers_adaptive_mult
+	//     with the value of the error at the chosen percentile.
+	//  4) Discard correspondences over the threshold.
+	//  This is useful to be conservative; yet remove the biggest errors.
+	if (!nh.getParam ("outliers_adaptive_order", input_.outliers_adaptive_order))
+		input_.outliers_adaptive_order = 0.7;
+
+	if (!nh.getParam ("outliers_adaptive_mult", input_.outliers_adaptive_mult))
+		input_.outliers_adaptive_mult = 2.0;
+
+	// If you already have a guess of the solution, you can compute the polar angle
+	// of the points of one scan in the new position. If the polar angle is not a monotone
+	// function of the readings index, it means that the surface is not visible in the
+	// next position. If it is not visible, then we don't use it for matching.
+	if (!nh.getParam ("do_visibility_test", input_.do_visibility_test))
+		input_.do_visibility_test = 0;
+
+	// no two points in laser_sens can have the same corr.
+	if (!nh.getParam ("outliers_remove_doubles", input_.outliers_remove_doubles))
+		input_.outliers_remove_doubles = 1;
+
+	// If 1, computes the covariance of ICP using the method http://purl.org/censi/2006/icpcov
+	if (!nh.getParam ("do_compute_covariance", input_.do_compute_covariance))
+		input_.do_compute_covariance = 0;
+
+	// Checks that find_correspondences_tricks gives the right answer
+	if (!nh.getParam ("debug_verify_tricks", input_.debug_verify_tricks))
+		input_.debug_verify_tricks = 0;
+
+	// If 1, the field 'true_alpha' (or 'alpha') in the first scan is used to compute the
+	// incidence beta, and the factor (1/cos^2(beta)) used to weight the correspondence.");
+	if (!nh.getParam ("use_ml_weights", input_.use_ml_weights))
+		input_.use_ml_weights = 0;
+
+	// If 1, the field 'readings_sigma' in the second scan is used to weight the
+	// correspondence by 1/sigma^2
+	if (!nh.getParam ("use_sigma_weights", input_.use_sigma_weights))
+		input_.use_sigma_weights = 0;
+
 }
 
 LaserOdometry::~LaserOdometry()
